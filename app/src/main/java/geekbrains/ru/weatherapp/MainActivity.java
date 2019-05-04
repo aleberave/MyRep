da85e3671d26a9346ac031f658e0bcbde0bcbdc1
aleberave <lionessa@km.ru>
2019-05-04 14:35:48 +1000

    lesson5

--- app/src/main/java/geekbrains/ru/weatherapp/MainActivity.java
+++ app/src/main/java/geekbrains/ru/weatherapp/MainActivity.java
@@ -2,101 +2,40 @@ package geekbrains.ru.weatherapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TEXT = "text";
-    public static final String BOOL_CBT = "bool_cbt";
-    public static final String BOOL_CBW = "bool_cbw";
-    public static final String BOOL_SAP = "bool_sap";
-
-    static final String CITY_NAME = "CITY_NAME";
-    private EditText enterCityName;
-    private CheckBox cbTemperature;
-    private CheckBox cbWind;
-    private Switch swAtmospherePressure;
-    private Button btnOk;
-
-    public static boolean SWITCH_ATMOSPHERE_PRESSURE = false;
-    public static boolean CHECK_BOX_TEMPERATURE = false;
-    public static boolean CHECK_BOX_WIND = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
+        setFragment();
     }
 
-    private void init() {
-        enterCityName = findViewById(R.id.enterCityName);
-        cbTemperature = findViewById(R.id.cbTemperature);
-        cbWind = findViewById(R.id.cbWind);
-        swAtmospherePressure = findViewById(R.id.swAtmospherePressure);
-        btnOk = findViewById(R.id.btnOk);
+    private void setFragment() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MainFragment mainFragment = (MainFragment) fragmentManager.findFragmentByTag(MainFragment.TAG);
+        Bundle bundle = new Bundle();
+        if (mainFragment == null) {
+            bundle.putString("", "");
+            mainFragment = MainFragment.init(bundle);
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container1, mainFragment, MainFragment.TAG);
+        transaction.commitAllowingStateLoss();
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        enterCityName.setText("");
-        CHECK_BOX_TEMPERATURE = false;
-        cbTemperature.setChecked(false);
-        CHECK_BOX_WIND = false;
-        cbWind.setChecked(false);
-        SWITCH_ATMOSPHERE_PRESSURE = false;
-        swAtmospherePressure.setChecked(false);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.container1);
+        mainFragment.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void btnOkClick(View view) {
-        btnOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (enterCityName.getText().length() == 0) {
-                    Toast.makeText(getApplicationContext(), R.string.sEnterCityName, Toast.LENGTH_LONG).show();
-                } else {
-                    Parsel parsel = new Parsel();
-                    parsel.text = enterCityName.getText().toString();
-
-                    parsel.checkBoxTemperature = MainActivity.CHECK_BOX_TEMPERATURE;
-                    parsel.checkBoxWind = MainActivity.CHECK_BOX_WIND;
-                    parsel.switchAtmospherePressure = MainActivity.SWITCH_ATMOSPHERE_PRESSURE;
-
-                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                    intent.putExtra(CITY_NAME, parsel);
-                    startActivity(intent);
-                }
-            }
-        });
-    }
-
-    public void onClickCheckBox(View view) {
-        boolean checkedBox = ((CheckBox) view).isChecked();
-        switch (view.getId()) {
-            case R.id.cbTemperature:
-                CHECK_BOX_TEMPERATURE = checkedBox;
-                break;
-            case R.id.cbWind:
-                CHECK_BOX_WIND = checkedBox;
-                break;
-        }
-    }
-
-    public void onClickSwitch(View view) {
-        boolean checkedSwitch = ((Switch) view).isChecked();
-        switch (view.getId()) {
-            case R.id.swAtmospherePressure:
-                SWITCH_ATMOSPHERE_PRESSURE = checkedSwitch;
-                break;
-        }
-    }
 }
